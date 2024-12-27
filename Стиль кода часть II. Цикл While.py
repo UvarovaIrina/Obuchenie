15521448a4dcde7d4006f568fabc968b9c7b0275
UvarovaIrina <yvarovairinka753@gmail.com>
2024-12-27 09:58:10 +0300

    Create Стиль кода часть II. Цикл While.py

---
+++ Стиль кода часть II. Цикл While.py
@@ -0,0 +1,21 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+neme = []
+while index < len(my_list):
+
+    if my_list[index] == 0:
+        index += 1
+    elif my_list[index] < 0:
+       
+        break
+    print(my_list[index])
+    index += 1
+
+
+
+
+
+
+
+
+
