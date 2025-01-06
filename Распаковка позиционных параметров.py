355ac49bc3e2d101240028df74166384775a8fde
UvarovaIrina <yvarovairinka753@gmail.com>
2025-01-06 20:39:01 +0300

    Create Распаковка позиционных параметров.py

---
+++ Распаковка позиционных параметров.py
@@ -0,0 +1,16 @@
+# values_list_2 = [54.32, 'Строка' ]
+# print_params(*values_list_2, 42)
+
+
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
+
+
+
